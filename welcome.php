67ea7b66c3e3a4470bb0f85d157a7a7499d3f9ff
Michael Kirkpatrick <michael@interchangedesign.com>
2017-11-23 17:39:33 -0800

    Added Welcome Message

--- welcome.php
+++ welcome.php
@@ -1,47 +1,50 @@
 <?php
 // Initialize
-$guest = false;
+$guest = true;
 include_once $_SERVER["DOCUMENT_ROOT"] . '/classes/initialize.php';
 
 // HTML Head
-$htmlHead = new htmlHead('Catalog.beer: The Internet\'s Beer Database');
+$htmlHead = new htmlHead('Welcome to Catalog.beer');
 echo $htmlHead->html;
 ?>
 <body>
 	<?php
 	// Navbar
-	echo $nav->navbar('Brewers');
-	
-	// First Name
-	$exploded = explode(' ', $userInfo->name);
-	if(count($exploded) == 2){
-		// Traditional Name
-		$firstName = $exploded[0];
-		$text = new Text(false, false, true);
-		$firstName = $text->get($firstName);
-	}else{
-		// Untraditional Name, let's log it
-		$errorLog = new LogError();
-		$errorLog->errorNumber = 'C3';
-		$errorLog->errorMsg = 'Untraditional Name';
-		$errorLog->badData = $userInfo->name;
-		$errorLog->filename = 'welcome.php';
-		$errorLog->write();
-	}
-	
-	// Email Text
-	if($userInfo->emailVerified){
-		$emailHTML = '<p>Your email address has been confirmed so you&#8217;re ready to go! Feel free to start browsing, adding beers, and updating information.</p>';
-	}else{
-		$emailHTML = '<p>You&#8217;re one step away from full-fledged membership. We&#8217;d like you to confirm your email for us so that we know you&#8217;re not a computer. Check your email. Once you&#8217;ve confirmed your email, you&#8217;ll be ready to add beers and update information across the site.</p>';
-	}
+	echo $nav->navbar('');
 	?>
 	<div class="container">
-    <div class="jumbotron">
-			<h1 class="display-3">Welcome, <?php echo $firstName; ?>!</h1>
-			<p class="lead">It&#8217;s great to have you as a part of the Catalog.beer community.</p>
-			<hr class="my-4">
-			<?php echo $emailHTML; ?>
+    <div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<h1>Welcome to Catalog.beer</h1>
+
+				<p class="text-muted"><small>Thursday, November 23, 2017</small></p>
+
+				<p>For digital entrepreneurs in the beer industry like myself, the key to getting started is having access to an authoritative, relatively complete database of beer and brewer data. With Catalog.beer, you will now have access to that trove of information for free.</p>
+
+				<p>For beer consumers, enthusiasts, and researchers, access to information about beer and brewers can help educate and inform. This database is here to help in that mission.</p>
+
+				<p>Today our catalog hosts <a href="/brewer">6,555 brewers</a> and <a href="/beer">70,713 beers</a>. I can&#8217;t wait to see how big it grows. And you can help. Catalog.beer has been designed as a platform that anyone can contribute to. Today, you can <a href="/brewer/add">add new brewers</a>, and add beers and locations to brewers. You can add and retrieve information both via the <a href="/">Catalog.beer</a> website and via our <a href="/api-docs">API</a>.</p>
+
+				<p>If you&#8217;re a developer, you can also contribute on GitHub. The project has been written in PHP, MySQL, and HTML with CSS help from Bootstrap.</p>
+
+				<ul>
+				<li><a href="https://github.com/michaelkirkpatrick/catalog-beer">Catalog.beer - GitHub</a></li>
+				<li><a href="https://github.com/michaelkirkpatrick/catalog-beer-api">Catalog.beer API - GitHub</a></li>
+				<li><a href="https://github.com/michaelkirkpatrick/catalog-beer-mysql">Catalog.beer MySQL - GitHub</a></li>
+				</ul>
+
+				<p>There&#8217;s more work to be done. Editing and search capabilities are two upcoming features that are in the near-term pipeline.</p>
+
+				<p>I welcome comments, questions, and most of all contributions.</p>
+
+				<p>Thanks for being a part of the community, and Happy Thanksgiving.</p>
+
+				<p>Sincerely,</p>
+
+				<p>Michael Kirkpatrick<br/>Founder, Catalog.beer<br><a href="/contact">Contact</a> / <a href="https://twitter.com/mekirkpatrick">Twitter</a></p>
+			</div>
+			<div class="col-md-2"></div>
 		</div>
   </div>
   <?php echo $nav->footer(); ?> 
