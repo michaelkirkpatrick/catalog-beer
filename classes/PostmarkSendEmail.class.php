6b25deeaa5d8e0701a828b2cae2e80b9ed0e62b7
Michael Kirkpatrick <michael@mekstudios.com>
2019-11-29 18:31:48 -0800

    Update PostmarkSendEmail.class.php
    Added replyTo

--- classes/PostmarkSendEmail.class.php
+++ classes/PostmarkSendEmail.class.php
@@ -9,13 +9,15 @@ class PostmarkSendEmail {
 	public $TextBody = '';
 	public $TrackOpens = false;
 	public $TrackLinks = 'None';
+	public $ReplyTo = '';
 	
-	function generateBody($to, $subject, $tag, $htmlBody, $textBody){
+	function generateBody($to, $subject, $tag, $htmlBody, $textBody, $replyTo){
 		$this->to = $to;
 		$this->subject = $subject;
 		$this->tag = $tag;
 		$this->HtmlBody = $htmlBody;
 		$this->TextBody = $textBody;
+		$this->ReplyTo = $replyTo;
 	}
 }	
 ?>
