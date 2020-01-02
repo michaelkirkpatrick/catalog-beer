e4b2ffbec32a8469f6ebf7b2b045a5ae2b2a470f
Michael Kirkpatrick <michael@mekstudios.com>
2020-01-02 15:31:25 -0800

    Updated Privacy and Terms
    Updated Privacy and Terms. Largely formatting changes with some additions for California and Nevada law in the Privacy Policy.

--- privacy.php
+++ privacy.php
@@ -7,6 +7,18 @@ include_once $_SERVER["DOCUMENT_ROOT"] . '/classes/initialize.php';
 $htmlHead = new htmlHead('Privacy Policy');
 echo $htmlHead->html;
 ?>
+<style>
+	h2 {
+		margin-top: 4rem;
+	}
+	h3 {
+		margin-top: 3rem;
+	}
+	h4 {
+		margin-top: 2rem;
+		color:#4b555e;
+	}
+</style>
 <body>
 	<?php
 	// Navbar
@@ -18,13 +30,14 @@ echo $htmlHead->html;
 			<!-- Empty Column -->
 			</div>
 			<div class="col-md-8">
-    		<h1>Privacy Policy</h1>
+    			<h1>Privacy Policy</h1>
 
-				<p>Last updated on October 30, 2018</p>
+				<p>Updated on January 2, 2020</p>
 
 				<p>Your privacy is important to us here at Catalog.beer. So we’ve developed a Privacy Policy that covers how we collect, use, disclose, and store your information. Please <a href="/contact">let us know</a> if you have any questions.</p>
 
 				<h2>Collection and Use of Personal Information</h2>
+				<hr>
 
 				<p>Personal information is data that can be used to identify or contact a single person.</p>
 
@@ -34,7 +47,7 @@ echo $htmlHead->html;
 
 				<h3>What personal information we collect</h3>
 
-				<p>When you create an Catalog.beer account, pay API fees, or contact us, we may collect a variety of information, including your name, credit card information, and email address.</p>
+				<p>When you create a Catalog.beer account, pay API fees, or contact us, we may collect a variety of information, including your name, credit card information, and email address.</p>
 
 				<h3>How we use your personal information</h3>
 
@@ -46,20 +59,29 @@ echo $htmlHead->html;
 				</ul>
 
 				<h2>Collection and Use of Non-Personal Information</h2>
+				<hr>
 
 				<p>We also collect data in a form that does not, on its own, permit direct association with any specific individual. We may collect, use, transfer, and disclose non-personal information for any purpose. The following are some examples of non-personal information that we collect and how we may use it:</p>
 
 				<h3>Website User Tracking</h3>
 
 				<p>We may collect information regarding customer activities on our website. This information is aggregated and used to help us provide more useful information to our customers and to understand which parts of our website and services are of most interest.</p>
+				
+				<h3>Do Not Track</h3>
+
+				<p>Catalog.beer does not track its customers over time and across third party websites to provide targeted advertising and therefore does not respond to Do Not Track (DNT) signals. However, some third party sites do keep track of your browsing activities when they serve you content, which enables them to tailor what they present to you.</p>
+
+				<p>Third parties that have content embedded on Catalog.beer’s websites, such as Google&#8217;s reCAPTCHA, set cookies on a user’s browser and/or obtain information about the fact that a web browser visited a specific Catalog.beer webpage from a certain IP address. Third parties cannot collect any other personally identifiable information from Catalog.beer’s websites unless you provide it to them directly.</p>
 
 				<h2>Cookies and Other Technologies</h2>
+				<hr>
 
-				<p>Catalog.beer&#8217;s website, online services, interactive applications, and email messages may use &#8220;cookies&#8221;. These technologies help us better understand user behavior and tell us which parts of our websites people have visited. We treat information collected by cookies and other technologies as non‑personal information. However, to the extent that Internet Protocol (IP) addresses or similar identifiers are considered personal information by local law, we also treat these identifiers as personal information. Similarly, to the extent that non-personal information is combined with personal information, we treat the combined information as personal information for the purposes of this Privacy Policy.</p>
+				<p>Catalog.beer&#8217;s website, online services, interactive applications, and email messages may use &#8220;cookies&#8221;. We treat information collected by cookies and other technologies as non‑personal information. However, to the extent that Internet Protocol (IP) addresses or similar identifiers are considered personal information by local law, we also treat these identifiers as personal information. Similarly, to the extent that non-personal information is combined with personal information, we treat the combined information as personal information for the purposes of this Privacy Policy.</p>
 
 				<p>As is true of most internet services, we gather some information automatically and store it in log files. This information includes Internet Protocol (IP) addresses, browser type, and date/time stamp. We use this information to understand and analyze trends, to administer the site, to learn about user behavior on the site, and to improve our services.</p>
 
 				<h2>Service Providers</h2>
+				<hr>
 
 				<p>Catalog.beer integrates with services from the following providers:</p>
 
@@ -68,40 +90,48 @@ echo $htmlHead->html;
 				<p>Catalog.beer uses Google&#8217;s <a href="https://www.google.com/recaptcha/intro/invisible.html">invisible reCAPTCHA</a> on our account creation and contact pages to help prevent robots from creating accounts and sending us spam email respectively. You can find Google&#8217;s <a href="https://www.google.com/intl/en/policies/privacy/">Privacy Policy</a> and <a href="https://www.google.com/intl/en/policies/terms/">Terms of Service</a> at the aforementioned links.</p>
 
 				<h2>Others</h2>
+				<hr>
 
 				<p>It may be necessary − by law, legal process, litigation, and/or requests from public and governmental authorities within or outside your country of residence − for Catalog.beer to disclose your personal information. We may also disclose information about you if we determine that for purposes of national security, law enforcement, or other issues of public importance, disclosure is necessary or appropriate.</p>
 
 				<p>We may also disclose information about you if we determine that disclosure is reasonably necessary to enforce our terms and conditions or protect our operations or users. Additionally, in the event of a reorganization, merger, or sale we may transfer any and all personal information we collect to the relevant third party.</p>
 
 				<h2>Protection of Personal Information</h2>
+				<hr>
 
 				<p>Catalog.beer takes the security of your personal information very seriously. Catalog.beer&#8217;s online services protect your personal information during transit using encryption such as Transport Layer Security (TLS). When your personal data is stored by Catalog.beer, we use computer systems with limited access housed in facilities using physical security measures, operated by our service provider <a href="https://www.linode.com/security">Linode</a>. Your password is encrypted both when you enter and send it to access our services, and when it is stored in our database.</p>
 
 				<h2>Integrity and Retention of Personal Information</h2>
+				<hr>
 
 				<p>Catalog.beer makes it easy for you to keep your personal information accurate, complete, and up to date. We will retain your personal information for the period necessary to fulfill the purposes outlined in this Privacy Policy unless a longer retention period is required or permitted by law.</p>
 
 				<h2>Access to Personal Information</h2>
+				<hr>
 
 				<p>You can help ensure that your contact information and preferences are accurate, complete, and up to date by logging in to your account at <a href="/account">https://catalog.beer/account</a>. For other personal information we hold, we will provide you with access for any purpose including to request that we correct the data if it is inaccurate or delete the data if Catalog.beer is not required to retain it by law or for legitimate business purposes. We may decline to process requests that are frivolous/vexatious, jeopardize the privacy of others, are extremely impractical, or for which access is not otherwise required by local law. Access, correction, or deletion requests can be made through our <a href="/contact">contact form</a>.</p>
 
 				<h2>Children</h2>
+				<hr>
 
 				<p>We understand the importance of taking extra precautions to protect the privacy and safety of children. Children under the age of 13, or equivalent minimum age in the relevant jurisdiction, are not permitted to create their own Catalog.beer account. </p>
 
 				<p>If we learn that we have collected the personal information of a child under 13, or equivalent minimum age depending on jurisdiction, we will take steps to delete the information as soon as possible.</p>
 
 				<h2>Third‑Party Sites and Services</h2>
+				<hr>
 
 				<p>Catalog.beer&#8217;s websites, applications, and services may contain links to third-party websites, products, and services.</p>
 
 				<p>Information collected by third parties, which may include such things as location data or contact details, is governed by their privacy practices. We encourage you to learn about the privacy practices of those third parties.</p>
 
 				<h2>Our Commitment to Your Privacy</h2>
+				<hr>
 
 				<p>To make sure your personal information is secure, we communicate our privacy and security guidelines to our team and strictly enforce privacy safeguards within the organization.</p>
 
 				<h2>Privacy Questions</h2>
+				<hr>
 
 				<p>If you have any questions or concerns about Catalog.beer’s Privacy Policy or data processing or if you would like to make a complaint about a possible breach of local privacy laws, please <a href="/contact">contact us</a>.</p>
 
@@ -109,13 +139,16 @@ echo $htmlHead->html;
 
 				<p>Catalog.beer may update its Privacy Policy from time to time. When we change the policy in a material way, a notice will be posted on our website along with the updated Privacy Policy.</p>
 
-				<h2>California Privacy Disclosures</h2>
+				<h2>State Specific Laws</h2>
+				<hr>
+				<h3>California</h3>
+				<p></p>The California Consumer Privacy Act provides California consumers with the right to obtain from Catalog.beer information about the personal information about you that we collect, use, and disclose. You can exercise your rights by <a href="/contact">contacting us</a>.</p>
 
-				<h3>Do Not Track</h3>
+				<p>If you choose to exercise your privacy rights, you have the right to not to receive discriminatory treatment or a lesser degree of service from Catalog.beer.</p>
 
-				<p>Catalog.beer does not track its customers over time and across third party websites to provide targeted advertising and therefore does not respond to Do Not Track (DNT) signals. However, some third party sites do keep track of your browsing activities when they serve you content, which enables them to tailor what they present to you.</p>
+				<h3>Nevada</h3>
+				<p>You have the right to opt-out of the sale of your personal information. Catalog.beer does not sell your personal information.</p>
 
-				<p>Third parties that have content embedded on Catalog.beer’s websites, such as Google&#8217;s reCAPTCHA and Analytics tracking code, set cookies on a user’s browser and/or obtain information about the fact that a web browser visited a specific Catalog.beer webpage from a certain IP address. Third parties cannot collect any other personally identifiable information from Catalog.beer’s websites unless you provide it to them directly.</p>
 			</div>
 			<div class="col-md-2">
 			<!-- Empty Column -->
--- terms.php
+++ terms.php
@@ -7,6 +7,18 @@ include_once $_SERVER["DOCUMENT_ROOT"] . '/classes/initialize.php';
 $htmlHead = new htmlHead('Terms of Use');
 echo $htmlHead->html;
 ?>
+<style>
+	h2 {
+		margin-top: 4rem;
+	}
+	h3 {
+		margin-top: 3rem;
+	}
+	h4 {
+		margin-top: 2rem;
+		color:#4b555e;
+	}
+</style>
 <body>
 	<?php
 	// Navbar
@@ -19,14 +31,18 @@ echo $htmlHead->html;
 			</div>
 			<div class="col-md-8">
 				<h1>Terms of Use</h1>
+				
+				<p>Updated on January 2, 2020</p>
 
 				<h2>Ownership of Site; Agreement to Terms of Use</h2>
+				<hr>
 
-				<p>These Terms and Conditions of Use (the “Terms of Use”) apply to the Catalog.beer web site located at https://catalog.beer, and all associated sites linked to that URL by Catalog.beer. The Site is the property of Michael Eason Kirkpatrick, doing business as Interchange Design (“Interchange Design”) in Los Angeles County, California. By using the site, you agree to these terms of use; if you do not agree, do not use the site.</p>
+				<p>These Terms and Conditions of Use (the “Terms of Use”) apply to the Catalog.beer web site located at https://catalog.beer, and all associated sites linked to that URL by Catalog.beer. The Site is the property of Michael Eason Kirkpatrick, doing business as Interchange Design (“Interchange Design”) in San Diego County, California. By using the site, you agree to these terms of use; if you do not agree, do not use the site.</p>
 
 				<p>Catalog.beer reserves the right, at its sole discretion, to change, modify, add or remove portions of these Terms of Use, at any time. It is your responsibility to check these Terms of Use periodically for changes. Your continued use of the Site following the posting of changes will mean that you accept and agree to the changes. As long as you comply with these Terms of Use, Catalog.beer grants you a personal, non-exclusive, non-transferable, limited privilege to enter and use the Site.</p>
 
 				<h2>Content</h2>
+				<hr>
 
 				<p>The content displayed on this website is licensed under a <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International license (CC BY 4.0)</a>. This license allows you to:</p>
 
@@ -51,6 +67,7 @@ echo $htmlHead->html;
 				<p>You can read the <a href="https://creativecommons.org/licenses/by/4.0/legalcode">full license text</a> on the Creative Commons website.</p>
 
 				<h2>Your Use of the Site</h2>
+				<hr>
 
 				<p>You may not attempt to gain unauthorized access to any portion or feature of the Site, or any other systems or networks connected to the Site or to any Catalog.beer server, or to any of the services offered on or through the Site, by hacking, password “mining” or any other illegitimate means.</p>
 
@@ -61,22 +78,28 @@ echo $htmlHead->html;
 				<p>You may not forge headers or otherwise manipulate identifiers in order to disguise the origin of any message or transmittal you send to Catalog.beer on or through the Site or any service offered on or through the Site. You may not pretend that you are, or that you represent, someone else, or impersonate any other individual or entity.</p>
 
 				<p>You may not use the Site or any Content for any purpose that is unlawful or prohibited by these Terms of Use, or to solicit the performance of any illegal activity or other activity which infringes the rights of Catalog.beer or others.</p>
+				
+				<p>Please also see information about <a href="/api-usage">API Usage</a>.</p>
 
 				<h2>Accounts, Passwords and Security</h2>
+				<hr>
 
 				<p>Certain features or services offered on or through the Site may require you to open an account (including setting up an Catalog.beer account and password). You are entirely responsible for maintaining the confidentiality of the information you hold for your account, including your password, and for any and all activity that occurs under your account as a result of your failing to keep this information secure and confidential. You agree to notify Catalog.beer immediately of any unauthorized use of your account, password, or API key, or any other breach of security. You may be held liable for losses incurred by Catalog.beer or any other user of or visitor to the Site due to someone else using your Catalog.beer password or account as a result of your failing to keep your account information secure and confidential.</p>
 
 				<p>You may not use anyone else’s Catalog.beer password or account at any time without the express permission and consent of the holder of that Catalog.beer password or account. Catalog.beer cannot and will not be liable for any loss or damage arising from your failure to comply with these obligations.</p>
 
 				<h2>Privacy</h2>
+				<hr>
 
 				<p>Catalog.beer’s <a href="/privacy">Privacy Policy</a> applies to use of this Site, and its terms are made a part of these Terms of Use by this reference. To view Catalog.beer’s Privacy Policy, click <a href="/privacy">here</a>. Additionally, by using the Site, you acknowledge and agree that Internet transmissions are never completely private or secure. You understand that any message or information you send to the Site may be read or intercepted by others, even if there is a special notice that a particular transmission (for example, credit card information) is encrypted.</p>
 
 				<h2>Links to Other Sites and to the Catalog.beer Site</h2>
+				<hr>
 
 				<p>This Site may contain links to other independent third-party Web sites (“Linked Sites”). These Linked Sites are provided solely as a convenience to our visitors. Such Linked Sites are not under Catalog.beer’s control, and Catalog.beer is not responsible for and does not endorse the content of such Linked Sites, including any information or materials contained on such Linked Sites. You will need to make your own independent judgment regarding your interaction with these Linked Sites.</p>
 
 				<h2>Disclaimers</h2>
+				<hr>
 
 				<p>Catalog.beer does not promise that the site or any content, service or feature of the site will be error-free or uninterrupted, or that any defects will be corrected, or that your use of the site will provide specific results. The site and its content are delivered on an “as-is” and “as-available” basis. All information provided on the site is subject to change without notice. Catalog.beer cannot ensure that any files or other data you download from the site will be free of viruses or contamination or destructive features. Catalog.beer disclaims all warranties, express or implied, including any warranties of accuracy, non-infringement, merchantability and fitness for a particular purpose. Catalog.beer disclaims any and all liability for the acts, omissions and conduct of any third parties in connection with or related to your use of the site and/or any Catalog.beer services. You assume total responsibility for your use of the site and any linked sites. Your sole remedy against Catalog.beer for dissatisfaction with the site or any content is to stop using the site or any such content. This limitation of relief is a part of the bargain between the parties.</p>
 
@@ -85,16 +108,19 @@ echo $htmlHead->html;
 				<p>Catalog.beer reserves the right to do any of the following, at any time, without notice: (1) to modify, suspend or terminate operation of or access to the Site, or any portion of the Site, for any reason; (2) to modify or change the Site, or any portion of the Site, and any applicable policies or terms; and (3) to interrupt the operation of the Site, or any portion of the Site, as necessary to perform routine or non-routine maintenance, error correction, or other changes.</p>
 
 				<h2>Limitation of Liability</h2>
+				<hr>
 
 				<p>Except where prohibited by law, in no event will Catalog.beer be liable to you for any indirect, consequential, exemplary, incidental or punitive damages, including lost profits, even if Catalog.beer has been advised of the possibility of such damages.</p>
 
 				<p>If, notwithstanding the other provisions of these Terms of Use, Catalog.beer is found to be liable to you for any damage or loss which arises out of or is in any way connected with your use of the Site or any Content, Catalog.beer’s liability shall in no event exceed US$100.00. Some jurisdictions do not allow limitations of liability, so the foregoing limitation may not apply to you.</p>
 
 				<h2>Indemnity</h2>
+				<hr>
 
 				<p>You agree to indemnify and hold Catalog.beer harmless from any demands, loss, liability, claims or expenses (including attorneys’ fees), made against Catalog.beer by any third party due to or arising out of or in connection with your use of the Site.</p>
 
 				<h2>Violation of These Terms of Use</h2>
+				<hr>
 
 				<p>Catalog.beer may disclose any information we have about you (including your identity) if we determine that such disclosure is necessary in connection with any investigation or complaint regarding your use of the Site, or to identify, contact or bring legal action against someone who may be causing injury to or interference with (either intentionally or unintentionally) Catalog.beer’s rights or property, or the rights or property of visitors to or users of the Site, including Catalog.beer’s customers. Catalog.beer reserves the right at all times to disclose any information that Catalog.beer deems necessary to comply with any applicable law, regulation, legal process or governmental request. Catalog.beer also may disclose your information when Catalog.beer determines that applicable law requires or permits such disclosure, including exchanging information with other companies and organizations for fraud protection purposes.</p>
 
@@ -107,26 +133,28 @@ echo $htmlHead->html;
 				<p>If Catalog.beer does take any legal action against you as a result of your violation of these Terms of Use, Catalog.beer will be entitled to recover from you, and you agree to pay, all reasonable attorneys’ fees and costs of such action, in addition to any other relief granted to Catalog.beer. You agree that Catalog.beer will not be liable to you or to any third party for termination of your access to the Site as a result of any violation of these Terms of Use.</p>
 
 				<h2>Governing Law; Dispute Resolution</h2>
+				<hr>
 
 				<p>You agree that all matters relating to your access to or use of the Site, including all disputes, will be governed by the laws of the United States and by the laws of the State of California without regard to its conflicts of laws provisions. You agree to the personal jurisdiction by and venue in the state and federal courts in Los Angeles County, California, and waive any objection to such jurisdiction or venue. Any claim under these Terms of Use must be brought within one (1) year after the cause of action arises, or such claim or cause of action is barred. Claims made under the separate terms and conditions of purchase for goods and services are not subject to this limitation. No recovery may be sought or received for damages other than out-of-pocket expenses, except that the prevailing party will be entitled to costs and attorneys’ fees. In the event of any controversy or dispute between Catalog.beer and you arising out of or in connection with your use of the Site, the parties shall attempt, promptly and in good faith, to resolve any such dispute. If we are unable to resolve any such dispute within a reasonable time (not to exceed thirty (30) days), then either party may submit such controversy or dispute to mediation. If the dispute cannot be resolved through mediation, then the parties shall be free to pursue any right or remedy available to them under applicable law.</p>
 
 				<h2>Void Where Prohibited</h2>
+				<hr>
 
 				<p>Catalog.beer administers and operates the interchangedesign.com Site from its location in Long Beach, California USA. Although the Site is accessible worldwide, not all features, products or services discussed, referenced, provided or offered through or on the Site are available to all persons or in all geographic locations, or appropriate or available for use outside the United States. Catalog.beer reserves the right to limit, in its sole discretion, the provision and quantity of any feature, product or service to any person or geographic area. Any offer for any feature, product or service made on the Site is void where prohibited. If you choose to access the Site from outside the United States, you do so on your own initiative and you are solely responsible for complying with applicable local laws.</p>
 
 				<h2>Miscellaneous</h2>
+				<hr>
 
 				<p>You may not use or export or re-export any Content or any copy or adaptation of such Content, or any product or service offered on the Site, in violation of any applicable laws or regulations, including without limitation United States export laws and regulations.</p>
 
 				<p>If any of the provisions of these Terms of Use are held by a court or other tribunal of competent jurisdiction to be void or unenforceable, such provisions shall be limited or eliminated to the minimum extent necessary and replaced with a valid provision that best embodies the intent of these Terms of Use, so that these Terms of Use shall remain in full force and effect. These Terms of Use constitute the entire agreement between you and Catalog.beer with regard to your use of the Site, and any and all other written or oral agreements or understandings previously existing between you and Catalog.beer with respect to such use are hereby superseded and cancelled. Other than as provided in a purchase agreement you enter into with Catalog.beer, Catalog.beer will not accept any counter-offers to these Terms of Use, and all such offers are hereby categorically rejected. Catalog.beer’s failure to insist on or enforce strict performance of these Terms of Use shall not be construed as a waiver by Catalog.beer of any provision or any right it has to enforce these Terms of Use, nor shall any course of conduct between Catalog.beer and you or any other party be deemed to modify any provision of these Terms of Use. These Terms of Use shall not be interpreted or construed to confer any rights or remedies on any third parties.</p>
 
 				<h2>Feedback and Information</h2>
+				<hr>
 
 				<p>Any feedback you provide at this site shall be deemed to be non-confidential. Catalog.beer shall be free to use such information on an unrestricted basis.</p>
 
 				<p>The information contained in this web site is subject to change without notice.</p>
-
-				<p>Updated on November 4, 2017</p>
 			</div>
 			<div class="col-md-2">
 			<!-- Empty Column -->
